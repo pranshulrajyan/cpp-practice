80f086077ce726ef8f901897c6374de34276eb84
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-22 15:01:52 +0530

    modefied operators.cpp

--- operators.cpp
+++ operators.cpp
@@ -7,6 +7,12 @@ int main(){
     cout << "enter second number:" << endl;
     cin >> b;
     int sum = a + b;
+    int multiply = a * b;
+    int divide = a / b;
+    int subtract = a - b;
     cout << "the required sum is:" << sum << endl;
+    cout << "the required product is:" << multiply << endl;
+    cout << "the required quotient is:" << divide << endl;
+    cout << "the required difference is:" << subtract << endl;
     return 0;
 }
