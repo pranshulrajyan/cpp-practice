3fbea821e4f115f2f0e488ce30c66303160c1e28
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-22 14:38:48 +0530

    added a new file

---
+++ inputinC++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout << "enter a number:" << endl;
+    cin >> a;
+    cout << "enter your next number:" << endl;
+    cin >> b ;
+    cout << "the sum of the two given numbers is:" << a*b << endl;
+    return 0;
+}
