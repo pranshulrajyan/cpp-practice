68b930a69349de7f3825eec3f0b7d5995bb9d618
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-22 12:57:44 +0530

    added variables file and renamed code file

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int age = 25;
+    char grade = 'A';
+    float score = 93.66f;
+    cout << " the age of ram is " << age << "\n the grade secured by him is " << grade << "\n this corresponds to the score of " << score << endl;
+    cout << (age <= 20) << endl; // uses bool
+    cout << (age >=20) << endl; //uses bool 
+    return 0;
+}
