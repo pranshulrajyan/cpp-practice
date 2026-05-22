d545a816878b83a944b49bc92a5989ad479d550a
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-22 22:39:19 +0530

    updated the comments

--- loops.cpp
+++ loops.cpp
@@ -2,105 +2,105 @@
 using namespace std;
 
 //while loop
-// int main(){
-//     int i = 1;
-//     int n;
-//     cout << "enter a number";
-//     cin >> n;
-//     while (i <= n){
-//         cout << i << " ";
-//         i++;
-//         }
-//         cout << endl;
-//         return 0;
-// } 
+int main(){
+    int i = 1;
+    int n;
+    cout << "enter a number";
+    cin >> n;
+    while (i <= n){
+        cout << i << " ";
+        i++;
+        }
+        cout << endl;
+        return 0;
+} 
 // //for loop
 // //for(initialization; condition; increment/decrement)
-// int main(){
-//     int i = 1;
-//     int n;
-//     cout << "enter an integer";
-//     cin >> n;
-//     for(i =1; i <=n; i++){
-//         cout << i << " ";
-//     }
-//     cout << endl;
-//     return 0;
-// }
+int main(){
+    int i = 1;
+    int n;
+    cout << "enter an integer";
+    cin >> n;
+    for(i =1; i <=n; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
 // //sum of numbers from 1 to n:
-// int main(){ //for loop to find the sum of numbers from 1 to n
-//     int i;
-//     int sum = 0;
-//     int n;
-//     cout << "input an integer";
-//     cin >> n;
-//     for(i = 1; i <=n; i++){
-//         sum += i;
-//     }
-//     cout<< "the sum till the input integer is" << sum << endl;
-//     return 0;
-// }
-// int main(){ //while loop to find the sum of numbers from 1 to n
-//     int i = 1;
-//     int sum = 0;
-//     int n;
-//     cout << "input an integer";
-//     cin >> n;
-//     while(i <= n){
-//         sum += i;
-//         i++;
-//         if(i == 5){
-//             break; //break statement to exit the loop when i is equal to 5      
-//         }
-//     }
-//     cout << "the sum till the input integer is" << sum << endl;
-//     return 0;
-// }
-// //sum of all odd numbers from 1 to n:
-// int main(){ //for loop to find the sum of odd numbers from 1 to n
-//     int i = 1;
-//     int n;
-//     cout << "input integer";
-//     cin >> n;
-//     int oddSum = 0;
-//     for(i = 1; i <=n ; i++){
-//         if(i%2 != 0){
-//             oddSum += i;
-//         }
-//     }
-//     cout << "the sum of odd numbers till the input integer is" << oddSum << endl;
-//     return 0;
-// }
-// int main(){ //while loop to find the sum of odd numbers from 1 to n
-//     int i = 1;
-//     int n;
-//     cout << "input integer";
-//     cin >> n;
-//     int oddSum = 0;
-//     while(i <= n){
-//         if(i%2 != 0){
-//             oddSum += i;
-//         }
-//         i++;
-//     }
-//     cout << "the sum of odd numbers till the input integer is" << oddSum << endl;
-//     return 0;
-// }
+int main(){ //for loop to find the sum of numbers from 1 to n
+    int i;
+    int sum = 0;
+    int n;
+    cout << "input an integer";
+    cin >> n;
+    for(i = 1; i <=n; i++){
+        sum += i;
+    }
+    cout<< "the sum till the input integer is" << sum << endl;
+    return 0;
+}
+int main(){ //while loop to find the sum of numbers from 1 to n
+    int i = 1;
+    int sum = 0;
+    int n;
+    cout << "input an integer";
+    cin >> n;
+    while(i <= n){
+        sum += i;
+        i++;
+        if(i == 5){
+            break; //break statement to exit the loop when i is equal to 5      
+        }
+    }
+    cout << "the sum till the input integer is" << sum << endl;
+    return 0;
+}
+//sum of all odd numbers from 1 to n:
+int main(){ //for loop to find the sum of odd numbers from 1 to n
+    int i = 1;
+    int n;
+    cout << "input integer";
+    cin >> n;
+    int oddSum = 0;
+    for(i = 1; i <=n ; i++){
+        if(i%2 != 0){
+            oddSum += i;
+        }
+    }
+    cout << "the sum of odd numbers till the input integer is" << oddSum << endl;
+    return 0;
+}
+int main(){ //while loop to find the sum of odd numbers from 1 to n
+    int i = 1;
+    int n;
+    cout << "input integer";
+    cin >> n;
+    int oddSum = 0;
+    while(i <= n){
+        if(i%2 != 0){
+            oddSum += i;
+        }
+        i++;
+    }
+    cout << "the sum of odd numbers till the input integer is" << oddSum << endl;
+    return 0;
+}
 // // do-while loop
-//  int main(){
-//     int i = 1;
-//     int n;
-//     cout << " enter an integer";
-//     cin >> n;
-//     do{
-//         cout << i << " ";
-//         i++;
-//     } while(i <= n);
+ int main(){
+    int i = 1;
+    int n;
+    cout << " enter an integer";
+    cin >> n;
+    do{
+        cout << i << " ";
+        i++;
+    } while(i <= n);
       
-//     cout << endl;
-//     return 0;
+    cout << endl;
+    return 0;
 
-//     }
+    }
 // check whether a number is prime or not?
 int main(){
     int i = 2;
