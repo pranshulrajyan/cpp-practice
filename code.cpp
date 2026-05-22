2caf4d0917222f2adf1e4a861725bde129f942d4
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-22 12:31:00 +0530

    updated code.cpp

--- code.cpp
+++ code.cpp
@@ -2,8 +2,5 @@
 using namespace std;
 int main(){
     cout << "hiii" << endl; //normal print statement
-    cout << (5/float(2)) << endl; // typecasting
     return 0;
-}
-// '=' is used to assign value whereas '==' is used to check whether both sides are equal or not
-  
+}  
