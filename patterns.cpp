8ae7fad68d748b920c0ca1d72b58f587edbaba08
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-27 15:33:06 +0530

    solved questions on patterns

--- patterns.cpp
+++ patterns.cpp
@@ -172,3 +172,95 @@ int main(){
     return 0;
 }
 //pyramid pattern:
+int main(){
+    int n = 4;
+    for(int i = 0; i < n; i++){
+        for(int j =0; j < n-i-1; j++){
+            cout << " ";
+        }
+        for( int j = 1; j <= i+1; j++){
+            cout << j;
+        } 
+        for(int j = i; j >= 1; j--){
+            cout << j;
+        }
+        cout << endl;
+    
+        }
+    return 0;
+}
+//hollow diamond pattern:
+int main(){
+    int n = 4;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+        cout << "*";
+
+        if( i != 0) {
+            for(int j = 0; j < 2*i-1; j++){
+                cout << " ";
+            }
+            cout << "*";
+
+        }
+        
+       cout << endl;
+
+    }
+
+    for(int i = 0; i < n-1; i++){
+         for(int j = 0; j < i + 1 ; j++){
+            cout << " ";
+        }
+
+        cout << "*";
+
+        if(i != n-2) {
+             for(int j = 0; j < 2*(n-i)-5; j++){
+            cout << " ";
+        }
+
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+//butterfly pattern:
+int main(){
+    int n = 4; 
+    //upper loop
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
+        cout << "*";
+        }
+        
+        for(int j = 0; j < 2*(n-i)-2 ; j++){
+            cout << " ";
+        }
+    
+        for(int j = 0; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+}
+//lower loop
+    for(int i = n-1; i >=0; i--){
+        for(int j = 0; j <= i; j++){
+            cout << "*";
+        }
+
+        
+        for(int j = 0; j < 2*(n-i)-2 ; j++){
+                cout << " ";
+            }
+        
+
+        for(int j = 0; j <= i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    }
