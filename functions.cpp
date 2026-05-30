b12fa3b1a91274ce6b90ed79e0170ae6b7786383
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-30 11:04:55 +0530

    solved question

--- functions.cpp
+++ functions.cpp
@@ -68,3 +68,32 @@ int main(){
     //pass by reference means that we are passing the address of the variable to the function, so any changes made to the variable inside the function will affect the original variable outside the function.
     return 0;
 }
+//Given an array arr of n elements. The task is to reverse the given array. The reversal of array should be inplace.
+class Solution{
+public:
+void reverse(vector<int>& arr){
+        int n = arr.size();
+        for(int i = 0; i < n/2; i++){
+            swap(arr[i],arr[n-i-1]);
+        }
+         for(int i = 0; i < n; i++){
+            cout << arr[i] << " ";
+        }
+
+}
+};
+int main(){
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++){
+         cin >> arr[i];
+    }
+
+    Solution obj;
+    obj.reverse(arr);
+    cout << endl;
+    return 0;
+}
